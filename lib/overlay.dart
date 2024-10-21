80f372a8f164fc219d68d1025b2270d354b4df61
Dhanraj <dp914278@gmail.com>
2024-10-21 23:30:07 +0530

    i roughly works :D

--- lib/overlay.dart
+++ lib/overlay.dart
@@ -1,7 +1,17 @@
+import 'dart:io';
 import 'dart:isolate';
 import 'dart:ui';
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_accessibility_service/constants.dart';
+import 'package:flutter_accessibility_service/flutter_accessibility_service.dart';
 import 'package:flutter_overlay_window/flutter_overlay_window.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+import 'package:native_image_cropper/native_image_cropper.dart';
+import 'package:path_provider/path_provider.dart';
 
 class OverlayWidget extends StatefulWidget {
   const OverlayWidget({super.key});
@@ -15,12 +25,51 @@ class OverlayWidgetState extends State<OverlayWidget> {
   BoxShape _currentShape = BoxShape.circle;
   static const String _kPortNameHome = 'UI';
   SendPort? homePort;
+  String? path;
   String? messageFromOverlay;
+  final controller = CropController();
+  Uint8List? _imageBytes;
+
+  Future<void> loadImage(String path) async {
+    String imagePath = path; // Replace with your image path
+    Uint8List imageBytes = await convertImageToUint8List(imagePath);
+    setState(() {
+      _imageBytes = imageBytes;
+    });
+  }
+
+  Future<Uint8List> convertImageToUint8List(String imagePath) async {
+    File imageFile = File(imagePath);
+    Uint8List imageBytes = await imageFile.readAsBytes();
+    return imageBytes;
+  }
 
   @override
   void initState() {
     super.initState();
     if (homePort != null) return;
+  }
+
+  Future<String?> getLatestScreenshot() async {
+    // Try using MediaStore to get latest screenshot from Pictures/Screenshots directory.
+    final screenshotsDir =
+        Directory('/storage/emulated/0/Pictures/Screenshots');
+
+    if (await screenshotsDir.exists()) {
+      List<FileSystemEntity> files = screenshotsDir.listSync();
+
+      // Sort files by last modified time to get the latest one.
+      files.sort((a, b) {
+        return File(b.path)
+            .lastModifiedSync()
+            .compareTo(File(a.path).lastModifiedSync());
+      });
+
+      if (files.isNotEmpty) {
+        return files.first.path;
+      }
+    }
+    return null;
   }
 
   @override
@@ -29,6 +78,7 @@ class OverlayWidgetState extends State<OverlayWidget> {
       color: Colors.transparent,
       elevation: 0.0,
       child: GestureDetector(
+        behavior: HitTestBehavior.opaque,
         onTap: () async {
           if (_currentShape == BoxShape.rectangle) {
             await FlutterOverlayWindow.resizeOverlay(50, 100, true);
@@ -36,9 +86,20 @@ class OverlayWidgetState extends State<OverlayWidget> {
               _currentShape = BoxShape.circle;
             });
           } else {
+            FlutterAccessibilityService.performGlobalAction(
+              GlobalAction.globalActionTakeScreenshot,
+            );
+
+            await Future.delayed(const Duration(milliseconds: 500));
+            String? pth = await getLatestScreenshot();
+            path = pth;
+            loadImage(path!);
+            await Future.delayed(const Duration(milliseconds: 500));
+
             await FlutterOverlayWindow.resizeOverlay(
-              WindowSize.matchParent,
-              WindowSize.matchParent,
+              // WindowSize.matchParent,
+              // WindowSize.matchParent,
+              410, 860,
               false,
             );
             setState(() {
@@ -53,37 +114,86 @@ class OverlayWidgetState extends State<OverlayWidget> {
             shape: _currentShape,
           ),
           child: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
+            child:
+
+                //  Column(
+                //   mainAxisSize: MainAxisSize.min,
+                //   crossAxisAlignment: CrossAxisAlignment.center,
+                //   children: [
                 _currentShape == BoxShape.rectangle
-                    ? SizedBox(
-                        width: 200.0,
-                        child: TextButton(
-                          style: TextButton.styleFrom(
-                            backgroundColor: Colors.black,
+                    ? Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            height: 600,
+                            width: 400,
+                            child: CropPreview(
+                              controller: controller,
+                              mode: CropMode.rect,
+                              bytes: _imageBytes!,
+                              dragPointSize: 20,
+                              hitSize: 20,
+                              dragPointBuilder: (size, position) {
+                                if (position == CropDragPointPosition.topLeft) {
+                                  return CropDragPoint(
+                                      size: size, color: Colors.red);
+                                }
+                                return CropDragPoint(
+                                    size: size, color: Colors.blue);
+                              },
+                            ),
                           ),
-                          onPressed: () {
-                            homePort ??= IsolateNameServer.lookupPortByName(
-                              _kPortNameHome,
-                            );
-                            homePort?.send('Date: ${DateTime.now()}');
-                          },
-                          child: const Text("Send message to UI"),
-                        ),
+                          Row(
+                            children: [
+                              IconButton(
+                                icon: Icon(Icons.crop),
+                                onPressed: () async {
+                                  final croppedBytes = await controller.crop();
+                                  var tmpFile =
+                                      await _createTempFile(croppedBytes!);
+                                  var file = tmpFile;
+                                  final inputImage = InputImage.fromFile(file);
+                                  final textRecognizer = TextRecognizer(
+                                      script: TextRecognitionScript.latin);
+                                  final RecognizedText recognizedText =
+                                      await textRecognizer
+                                          .processImage(inputImage);
+                                  await Clipboard.setData(
+                                      ClipboardData(text: recognizedText.text));
+                                  print(recognizedText.text);
+                                  _currentShape = BoxShape.circle;
+                                  setState(() {});
+                                },
+                              ),
+                            ],
+                          )
+                        ],
                       )
                     : const SizedBox.shrink(),
-                _currentShape == BoxShape.rectangle
-                    ? messageFromOverlay == null
-                        ? const FlutterLogo()
-                        : Text(messageFromOverlay ?? '')
-                    : const FlutterLogo()
-              ],
-            ),
+            //   _currentShape == BoxShape.rectangle
+            //       ? messageFromOverlay == null
+            //           ? const FlutterLogo()
+            //           : Text(messageFromOverlay ?? '')
+            //       : const FlutterLogo()
+            // ],
+            // ),
           ),
         ),
       ),
     );
+  }
+
+  Future<File> _createTempFile(Uint8List imageData) async {
+    File tempFile = await convertUint8ListToTempFile(imageData, 'image.png');
+
+    return tempFile;
+  }
+
+  Future<File> convertUint8ListToTempFile(
+      Uint8List data, String fileName) async {
+    Directory tempDir = await getTemporaryDirectory();
+    File tempFile = File('${tempDir.path}/$fileName');
+    await tempFile.writeAsBytes(data);
+    return tempFile;
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import device_info_plus
+import native_image_cropper_macos
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  NativeImageCropperPlugin.register(with: registry.registrar(forPlugin: "NativeImageCropperPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
